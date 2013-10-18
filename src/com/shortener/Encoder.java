894b44ff570afdba7a52aa30e7355193e3bcc50f
Denis Polyakov <den1s.polyakov1@gmail.com>
2013-10-18 17:00:02 +0300

    Update Encoder.java

--- src/com/shortener/Encoder.java
+++ src/com/shortener/Encoder.java
@@ -5,28 +5,20 @@ public class Encoder {
     private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789";
     private static final int    BASE     = ALPHABET.length();
 
-    public static String encode(int num)
-    {
+    public static String encode(int num) {
         StringBuilder sb = new StringBuilder();
-
-        while ( num > 0 )
-        {
+        while ( num > 0 ) {
             sb.append( ALPHABET.charAt( num % BASE ) );
             num /= BASE;
         }
-
         return sb.reverse().toString();
     }
 
-    public static int decode(String str)
-    {
+    public static int decode(String str) {
         int num = 0;
-
-        for ( int i = 0, len = str.length(); i < len; i++ )
-        {
+        for ( int i = 0, len = str.length(); i < len; i++ ) {
             num = num * BASE + ALPHABET.indexOf( str.charAt(i) );
         }
-
         return num;
     }
 
